8e8c030d39be921077398ff64d94bf30475ae532
danielleTamUiPath <145396303+danielleTamUiPath@users.noreply.github.com>
2024-03-22 16:39:10 -0300

    Create GetToken.ps1

---
+++ BlankProcess4/GetToken.ps1
@@ -0,0 +1,10 @@
+function Get-Token {
+    $body = @{client_id = 'd1fb1f0a-5e14-4e54-ba83-7370f9942846'
+        grant_type      = 'client_credentials'
+        scope           = 'OR.TestSets'
+        client_secret   =  'TVgsSyR)(o)X5LEB'
+    } 
+    
+    $tokenResponse = Invoke-RestMethod -Method POST -Uri 'https://staging.uipath.com/identity_/connect/token' -Headers $header -Body $body -ContentType "application/x-www-form-urlencoded"
+    return $tokenResponse.access_token
+}
